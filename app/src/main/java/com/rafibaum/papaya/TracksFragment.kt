395ff1678bf2f0ffd4f9ea646160ea660d22f142
Rafi Baum <rafi@ukbaums.com>
2020-07-13 17:54:51 -0400

    Implemented track listing

--- app/src/main/java/com/rafibaum/papaya/TracksFragment.kt
+++ app/src/main/java/com/rafibaum/papaya/TracksFragment.kt
@@ -8,8 +8,10 @@ import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
 import androidx.lifecycle.observe
 import androidx.navigation.fragment.navArgs
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.google.android.material.transition.MaterialContainerTransform
 import com.rafibaum.papaya.albums.AlbumStore
+import com.rafibaum.papaya.albums.TracksAdapter
 import kotlinx.android.synthetic.main.fragment_tracks.*
 
 class TracksFragment : Fragment() {
@@ -39,10 +41,25 @@ class TracksFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        tracksList.setHasFixedSize(true)
+
+        postponeEnterTransition()
+        tracksList.viewTreeObserver.addOnPreDrawListener {
+            startPostponedEnterTransition()
+            true
+        }
+
         albumStore.albums.observe(viewLifecycleOwner) {
-            textView.text = it[args.albumIndex].name
-            artistText.text = it[args.albumIndex].artist
-            imageView.setImageURI(it[args.albumIndex].cover)
+            val album = it[args.albumIndex]
+            tracksAlbumName.text = album.name
+            tracksAlbumArtist.text = album.artist
+            tracksAlbumCover.setImageURI(album.cover)
+
+            val adapter = TracksAdapter(album)
+            tracksList.adapter = adapter
+            tracksList.layoutManager = LinearLayoutManager(context)
+            tracksList.isNestedScrollingEnabled = false
         }
     }
 }
---
+++ app/src/main/java/com/rafibaum/papaya/albums/TracksAdapter.kt
@@ -0,0 +1,31 @@
+package com.rafibaum.papaya.albums
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.rafibaum.papaya.R
+
+class TracksAdapter(private val album: Album) : RecyclerView.Adapter<TracksAdapter.ViewHolder>() {
+
+    inner class ViewHolder(trackView: View) : RecyclerView.ViewHolder(trackView) {
+        val trackPosition: TextView = trackView.findViewById(R.id.trackPosition)
+        val trackName: TextView = trackView.findViewById(R.id.trackName)
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val inflater = LayoutInflater.from(parent.context)
+        val trackView = inflater.inflate(R.layout.track, parent, false)
+        return ViewHolder(trackView)
+    }
+
+    override fun getItemCount(): Int = album.tracks.size
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val track = album.tracks[position]
+        holder.trackPosition.text = track.position.toString()
+        holder.trackName.text = track.name
+    }
+
+}
